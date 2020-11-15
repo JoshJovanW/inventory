6b16855fe30f01ca2ccff5fdd458f899597022d4
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2020-11-15 07:28:41 +0700

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,1 +1,21 @@
+class Partition:
+    def __init__(self, floor, room):
+        self.floor = floor
+        self.room = room
+        self.product = []
+
+    def get_floor(self):
+        return f"floor number {self.floor}"
+
+    def get_room(self):
+        return f"room number {self.room}"
+
+    def get_products(self):
+        return f"the contents inside the room are {self.product}"
+
+    def add_products(self, product):
+        self.product.append(product)
+
+    def withdraw_products(self, product):
+        self.product.remove(product)
 
