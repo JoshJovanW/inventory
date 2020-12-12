72229237f2cfd146679381b8c9bb88a91bcb481f
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2020-12-12 17:30:28 +0700

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,18 @@
+def Menu():
+
+    print(" Programme to simulate a virtual multi - floored warehouse called tesco.\n")
+
+    print("Abilities:\n")
+
+    print("- Make Storage partitions.\n") 
+
+    print("- Add or Withdraw products to an partition")
+
+    print("- Check the items inside a partition.\n")
+
+    print("- Check the quantity of an item inside a partitions.\n")
+
+    print("- Look at the log of your activity.\n")
+
+
+    
