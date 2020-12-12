814089b760edd6374601e68260f1b512a58a2cef
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2020-12-12 17:28:15 +0700

    Update product.py

--- product.py
+++ product.py
@@ -1,1 +1,21 @@
+class Product:
+    def __init__(self, name, code, quantity):
+        self.name = name
+        self.code = code
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"product : {self.name}, stock : {self.quantity} "
+
+    def add_quantity(self, quantity):
+        self.quantity += quantity
+
+    def add_name(self, name):
+        self.name = name
+        
+    def add_code(self, code):
+        self.code = code
+
+
+
 
