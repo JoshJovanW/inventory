6a9e0461f8792c114b34e54490417dd0bf53f111
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2020-12-12 17:27:31 +0700

    Create abbreviations.py

---
+++ abbreviations.py
@@ -0,0 +1,15 @@
+def abrev():
+
+    print(" - SP"  +  "              "  +  "Storage Partition")
+
+    print(" - ADD" +  "             "   +  "Add products")
+
+    print(" - WD"  +  "              "  +  "Withdraw products")
+
+    print(" - CQ"  +  "              "  +  "Check quantity")
+
+    print(" - CI"  +  "              "  +  "Check items")
+
+    print(" - LOG" +  "             "   +  "Print log")
+
+    print(" - END" +  "             "   +  "End the programme")
