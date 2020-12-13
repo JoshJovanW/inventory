26242942d4b3c83e1c041019ddf046cfd1ea26b3
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2020-12-13 12:15:46 +0700

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,22 +1,11 @@
 class Partition:
-    def __init__(self, name, floor, room):
+    def __init__(self, name, floor):
         self.name = name
         self.floor = floor
-        self.room = room
         self.products = {}
 
     def __str__(self):
-        return f"name : {self.name}, room : {self.room}, floor : {self.floor}"
-
-    def set_name(self, name):
-        self.name = name
-
-    def set_floor(self, floor):
-        self.floor = floor 
-
-    def set_room(self, room):
-        self.room = room
-
+        return f"name : {self.name}, floor : {self.floor}"
 
     def get_products(self):
         return f"the contents inside the room are {self.product}"
@@ -26,3 +15,5 @@ class Partition:
 
     def withdraw_products(self, code):
         self.product.pop(code)
+
+    
