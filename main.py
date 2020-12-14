d6c2ccb3af064c2b195fe827bd550b0a070a814b
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2020-12-14 19:58:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from menu import Menu
+rom menu import Menu
 
 from partition import Partition
 
@@ -13,13 +13,6 @@ Menu()
 tesco = Warehouse()
 
 finish_process = False
-
-object_1 = Product("object1", 123, 10)
-
-object_2 = Product("object2", 124, 7)
-
-
-
 
 while finish_process == False:
     print("what do you want to do? Write the abbreviations for the action (look at the table below)\n")
@@ -46,9 +39,6 @@ while finish_process == False:
         for partition in tesco.partitions:
             print(partition.floor, "     ", partition.name)
 
-        storage_partition.add_products(object_1)
-        storage_partition.add_products(object_2)
-
     elif action == "ADD":
         print("In which partition do you want to add the product? (look at below to see existing partitions)\n")
 
@@ -66,47 +56,139 @@ while finish_process == False:
         for partition in tesco.partitions:
             if partition_name == partition.name and partition_floor == partition.floor:
                 print("what is the code of the product? (it will check if the product already exist)")
-                product_code = input()
+                product_code = int(input())
 
-                for key in partition.products:
-                    if product_code == partition.products[key].code:
-                        print("This product exists.")
-                        print("How much quantity do you want to add?")
-                        product_quantity = int(input())
+                if partition.products != {}:
+                    for key in partition.products:
+                        if product_code == partition.products[key].code:
+                            print("This product exists.")
+                            print("How much quantity do you want to add?")
+                            product_quantity = int(input())
 
-                        partition.products[key].add_quantity(product_quantity)
-                        print("The quantity of the product have been added")
-                        break
+                            partition.products[key].add_quantity(
+                                product_quantity)
+                            print("The quantity of the product have been added")
+                            break
 
-                    else:
-                        print("what is the name of the product? ")
-                        new_product_name = input()
+                else:
+                    print("The product under that code doesn't exist. create a new one.")
 
-                        print("what is the code of the product? ")
-                        new_product_code = input()
+                    print("what is the name of the product? ")
+                    new_product_name = input()
 
-                        print("what is the quantity of the product? ")
-                        new_product_quantity = int(input())
+                    print("what is the code of the product? ")
+                    new_product_code = int(input())
 
-                        new_product = Product(
-                            new_product_name, new_product_code, new_product_quantity)
+                    print("what is the quantity of the product? ")
+                    new_product_quantity = int(input())
 
-                        partition.add_products(new_product)
-                        print("THe product has been added.")
-                        break
+                    new_product = Product(new_product_name, new_product_code, new_product_quantity)
+
+                    partition.add_products(new_product)
+                    print("THe product has been added.")
+                    break
 
             else:
                 print("This is not a valid partition.")
                 break
 
     elif action == "WD":
-        pass
+        print("In which partition do you want to withdraw the product? (look at below to see existing partitions)\n")
+
+        print("existing partitions: \n")
+        print("Floor" + "     " + "Name\n")
+
+        for partition in tesco.partitions:
+            print(partition.floor, "     ", partition.name)
+
+        partition_name = input()
+
+        print("Which floor is the partition in?")
+        partition_floor = int(input())
+
+        for partition in tesco.partitions:
+            if partition_name == partition.name and partition_floor == partition.floor:
+                print("what is the code of the product?")
+                product_code = int(input())
+
+                if partition.products != {}:
+                    for key in partition.products:
+                        if product_code == partition.products[key].code:
+                            print("do you want to remove a specific quantity or all of it?\n")
+                            print("Type 'specific' for the first option and 'all' for the latter")
+                            withdraw_option = input()
+
+                            if withdraw_option == "specific":
+                                print("How much quantity do you want to remove?")
+                                quantity_to_remove = int(input())
+
+                                partition.products[key].remove_quantity(
+                                    quantity_to_remove)
+                                print("The quantity of the product has been removed")
+                                break
+
+                            elif withdraw_option == "all":
+                                partition.withdraw_products(product_code)
+                                print("Your product has been withdrawed.")
+                                break
+
+                            else:
+                                print("The option is not available please try again. ")
+                                break
+
+                        else:
+                            print("The product does'nt exist please try another one.")
+                            break
+
+                else:
+                    print("There are no products in this partition with that code please try another one.")
+                    break
+
+            else:
+                continue
+                print("That is not a valid partition")
+                break
 
     elif action == "CQ":
-        pass
+        total_quantity_of_item = 0
+
+        print("what is the code of the product?")
+        product_code = int(input())
+
+        for partition in tesco.partitions:
+            for key in partition.products:
+                if product_code == partition.products[key].code:
+                    total_quantity_of_item += partition.products[key].quantity
+
+        print("The total quantity of the item under that code is" + total_quantity_of_item)
+        print("note if the quantity is 0 then it could be the product under that code does'nt exist")
+
 
     elif action == "CI":
-        pass
+        print("In which partition do you want to add the product? (look at below to see existing partitions)\n")
+
+        print("existing partitions: \n")
+        print("Floor" + "     " + "Name\n")
+
+        for partition in tesco.partitions:
+            print(partition.floor, "     ", partition.name)
+
+        partition_name = input()
+
+        print("Which floor is the partition in?")
+        partition_floor = int(input())
+
+        for partition in tesco.partitions:
+            if partition_name == partition.name and partition_floor == partition.floor:
+                if partition.products != {}:
+                    for key in partition.products:
+                        print(partition.products[key].name)
+                        break
+                else:
+                    print("there are no products in this partition.")
+                    break
+            else:
+                print("this is not a valid partition.")
 
     elif action == "LOG":
         pass
@@ -118,4 +200,3 @@ while finish_process == False:
     else:
         print("This action is not available. Pls try again.")
         finish_process = True
-
