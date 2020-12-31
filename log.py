3dbc063e5ca7cf58940de0996285e88d0426b04c
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2020-12-31 12:44:09 +0700

    Update log.py

--- log.py
+++ log.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Log:
     def __init__(self):
         self.records = []
@@ -5,5 +7,9 @@ class Log:
     def __str__(self):
         return f"{self.records}"
 
-    def add_record(self, record):
-        self.records.append(record)
+    def add_record(self, description):
+        time = datetime.now()
+        date_and_time = time.strftime("%d/%m/%Y %H:%M:%S")
+        statement = ("[" + date_and_time + "]" + "  " + description)
+        self.records.append(statement)
+
